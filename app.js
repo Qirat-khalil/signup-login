fac5cf5aa8833df9e5d5c9d62b700df4ec316a7d
mariyam saleem <mariyamsaleem87@gmail.com>
2025-07-25 12:23:02 +0500

    done

--- app.js
+++ app.js
@@ -70,10 +70,16 @@ let google = document.querySelector(".goobtn")
 
 google && google.addEventListener("click", async () => {
     try {
+        let redirect = window.location.hostname === "127.0.01"
+        ? window.location.origin + "/post.html"
+        : window.location.origin + "/signup-login"
+
         const { error } = await merg.auth.signInWithOAuth({
             provider: 'google',
             options: {
                 redirectTo: window.location.origin + '/post.html',
+                redirectTo: "https://github.com/Qirat-khalil/signup-login",
+                redirectTo: redirect,
                 queryParams: { access_type: 'offline', prompt: 'consent' },
             },
         });
